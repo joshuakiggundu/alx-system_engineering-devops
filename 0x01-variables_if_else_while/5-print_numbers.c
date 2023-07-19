db4dc263c32e7b52fa8faa31b5d4f46bc194cb41
joshuakiggundu <kiggundujoshua256@gmail.com>
2023-07-19 01:18:31 -0700

    coding requires time

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -prrint numbers in base ten
+ *
+ * Return:  always
+ *
+ */
+int main(void)
+{
+	char i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
